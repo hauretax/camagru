84080e083459e0075cfd8858b6747a12e7d17c61
Huggo TRICOTTET <hutricot@e3r4p23.42.fr>
2019-06-05 18:09:55 +0200

    ff

--- config/setup.php
+++ config/setup.php
@@ -1,45 +1,48 @@
 <?PHP
-require_once 'database.php';
+require_once "database.php";
 
+$servername = "db";
+$username = "root";
+$password = "test";
 
-function set($dbh,$dbname)
-{
-  $sql  = "CREATE DATABASE IF NOT EXISTS ".$dbname;
-  $result = $dbh->exec($sql);
-
-  $sql ="USE ".$dbname;
-  $resulte = $dbh->exec($sql);
-
-    $sql  = "CREATE TABLE `user` (
-        `id` int(11) NOT NULL,
-        `login` varchar(32) NOT NULL,
-        `password` varchar(128) NOT NULL,
-        `mail` varchar(255) DEFAULT NULL,
-        `cle` varchar(255) DEFAULT NULL,
-        `actif` varchar(255) DEFAULT NULL
-      ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-    $resulte = $dbh->exec($sql);
-
-    $sql = "CREATE TABLE `pictur` (
-        `id` int(11) NOT NULL,
-        `login` varchar(255) NOT NULL,
-        `file` varchar(255) NOT NULL,
-        `lik` int(11) DEFAULT NULL,
-        `who_like` text NOT NULL,
-        `comment` text
-      ) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
-    $resulte = $dbh->exec($sql);
-}/*
-$dsn = "mysql:host=".$DB_HOST;
-$db = new PDO( $dsn,$DB_USER,$DB_PASSWORD);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-setup($db,$DB_NAME);
-echo 'setup completed'.PHP_EOL;*/
-
-$bdd = new database();
-$bdd->connexion();
-$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-set($bdd, 'camagru');
-
-echo 'setup completed';
+// Create connection
+try {
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD);
+$sql = "CREATE DATABASE camagru";
+$conn->query($sql) === TRUE ;
+}
+catch (Exception $e) {
+    echo "Error creating database: " . $e;
+}
+$conn->close();
+try {
+$bdd = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+$bdd->query("CREATE TABLE `user` (
+  `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `login` varchar(32) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `mail` varchar(255) DEFAULT NULL,
+  `cle` varchar(255) DEFAULT NULL,
+  `actif` varchar(255) DEFAULT NULL
+)");
+$bdd->query("ALTER TABLE user CONVERT TO CHARACTER SET utf8;");
+}
+catch (Exception $e) {
+    echo "Error creating database: " . $e;
+}
+try {
+  $bdd = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+  $bdd->query("CREATE TABLE `pictur` (
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `login` varchar(255) NOT NULL,
+    `file` varchar(255) NOT NULL,
+    `lik` int(11) DEFAULT NULL,
+    `who_like` text NOT NULL,
+    `comment` text
+  )");
+  $bdd->query("ALTER TABLE user CONVERT TO CHARACTER SET utf8;");
+  }
+  catch (Exception $e) {
+      echo "Error creating database: " . $e;
+  }
 ?>
